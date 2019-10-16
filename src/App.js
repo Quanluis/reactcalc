5e95b9fbbc759f359525c050d3eb130f3517d815
Quanluis <quanluis@protonmail.com>
2019-10-15 23:46:13 -0400

    Added calculator grid

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor(){
+    super()
+      this.state  = {
+        
+    }
+  }
+
+  addNum = () => {
+    this.setState({
+      
+    })
+  }
+
+  render() {
+  return (
+    <div>
+    <div className="grid-container">
+      <div className="grid-item">1</div>
+      <div className="grid-item">2</div>
+      <div className="grid-item">3</div>  
+      <div className="grid-item">4</div>
+      <div className="grid-item">5</div>
+      <div className="grid-item">6</div>  
+      <div className="grid-item">7</div>
+      <div className="grid-item">8</div>
+      <div className="grid-item">9</div>  
+</div>
+    <p>Result: {}</p>
+    </div>
+  );
+  }
+}
+
+export default App;
